0c0c12c14801c19ab74c8b5f31381e541ad62fd8
DuvanRiPe <Duvanriverape96@gmail.com>
2022-04-18 20:08:45 -0500

    commit

--- Admin/index.php
+++ Admin/index.php
@@ -20,34 +20,43 @@ if (!isset($_SESSION['codigo_rol'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Login</title>
 </head>
 <body>
     
-    <div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh">
-        <form action="index.php" method="post" class="border shadow p-3 rounded" style="width: 450px;" >
-
-            <h1 class="text-center p-3 "  >Login Admin</h1>
-            <?php
-                if (isset($_REQUEST['btninicio'])) {
-                    include "validarLogin.php";
-                }
-                ?>
-            <div class="mb-3">
-                <label for="usuario_admin" class="form-label">Nombre de Usuario</label>
-                <input type="text" class="form-control" name="usuario_admin" id="usuario_admin" aria-describedby="emailHelp">
+    <div class="p-5">
+        <div class="card row " style="width: 30rem; margin: auto auto">
+            <div class="d-flex align-items-center justify-content-center">
+                <img src="img/Logo.png" class="card-img-top img1" alt="...">    
             </div>
+            <div class="card-body">
+                <form action="index.php" method="post" >
 
-            <div class="mb-3">
-                <label for="contraseña_admin" class="form-label">Contraseña</label>
-                <input type="password" class="form-control" name="contraseña_admin" id="contraseña_admin">
+                    <h1 class="text-center p-3 ">Login Admin</h1>
+                    <?php
+                        if (isset($_REQUEST['btninicio'])) {
+                            include "validarLogin.php";
+                        }
+                        ?>
+                    <div class="">
+                        <label for="usuario_admin" class="form-label">Nombre de Usuario</label>
+                        <input type="text" class="form-control" name="usuario_admin" id="usuario_admin" aria-describedby="emailHelp">
+                    </div>
+
+                    <div class="">
+                        <label for="contraseña_admin" class="form-label">Contraseña</label>
+                        <input type="password" class="form-control" name="contraseña_admin" id="contraseña_admin">
+                    </div>
+                        <br>    
+                    <div class="text-center">
+                        <button name="btninicio" type="submit" class="btn btn-primary">Iniciar Sesion</button>
+                    </div>
+
+                </form>
             </div>
-            
-            <button name="btninicio" type="submit" class="btn btn-primary">Iniciar Sesion</button>
-
-        </form>
+        </div>
     </div>
-
 
     <script src="js/bootstrap.bundle.min.js"></script>
 </body>
